16f2e463ada4b082191f53b65504f6d478ecfe96
Safe-Land-Tourism <smrasel15@yahoo.com>
2026-09-05 17:00:41 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// WhatsApp-এ ডাইরেক্ট প্যাকেজের নামসহ মেসেজ পাঠানোর ফংশন
-function sendWhatsApp(packageName) {
-    var phoneNumber = "971504915900"; // আপনার WhatsApp নাম্বার
-    var message = "Hello, I am interested in booking: " + packageName;
-    var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
-    window.open(url, '_blank');
-}
